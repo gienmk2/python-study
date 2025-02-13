9904714c6af1bded698db5d8f5f0bb4a69a79d6d
gienmk2 <amuro14@hotmail.com>
2025-02-14 07:35:37 +0900

    [ADD] recursive function etc

--- lesson_total/a.py
+++ lesson_total/a.py
@@ -1,10 +1,10 @@
 import os
 from PIL import Image
 
-list = os.listdir(r"C:\Users\PC\Desktop\python lesson\sample\photo")
+list = os.listdir(r"C:\Users\PC\github\python_lesson\lesson_total\sample\photo")
 
 for i in list:
-    pa = os.path.join(r"C:\Users\PC\Desktop\python lesson\sample\photo", i)
+    pa = os.path.join(r"C:\Users\PC\github\python_lesson\lesson_total\sample\photo", i)
     if os.path.getsize(pa) >= 204800:
         img = Image.open(pa)
         img.thumbnail((500,400))
